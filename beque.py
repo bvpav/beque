fc91d4e81966bb4712c2f71218b94196e2b2fba9
bvpav <bvpav@tutanota.com>
2022-06-20 23:19:53 +0300

    added deque and functions

--- beque.py
+++ beque.py
@@ -1,3 +1,4 @@
+import collections
 import re
 import sys
 
@@ -10,16 +11,77 @@ else:
 program = re.sub(r'#.*\n', '', program)
 program = program.split()
 
+deque = collections.deque()
+
+
+def push(left: bool, value):
+    if left:
+        deque.appendleft(value)
+    else:
+        deque.append(value)
+
+
+def pop(left: bool):
+    if left:
+        return deque.popleft()
+    else:
+        return deque.pop()
+
+
+funcs = {}
+
+
+def func(f_or_name):
+    def inner(f, name):
+        funcs[name] = f
+        return f
+
+    if callable(f_or_name):
+        return inner(f_or_name, f_or_name.__name__)
+
+    return lambda f: inner(f, f_or_name)
+
+
+@func('+')
+def add(left):
+    a = pop(left)
+    b = pop(left)
+    push(left, a + b)
+
+
+@func('-')
+def sub(left):
+    a = pop(left)
+    b = pop(left)
+    push(left, b - a)
+
+
+@func('print')
+def print_func(left):
+    value = pop(left)
+    print(value)
+
+
 ip = 0
 while ip < len(program):
+    print(deque)
     instruction = program[ip]
     if instruction.startswith('.'):
-        left = False
+        left = True
         instruction = instruction[1:]
     elif instruction.endswith('.'):
-        left = True
+        left = False
         instruction = instruction[:-1]
     else:
         raise RuntimeError(f'missing position at {ip}: {instruction!r}')
+
     print(f'{ip=} {instruction=} {left=}')
+
+    if instruction.isnumeric():
+        push(left, int(instruction))
+    elif instruction in funcs:
+        funcs[instruction](left)
+    else:
+        raise NameError(instruction)
+
     ip += 1
